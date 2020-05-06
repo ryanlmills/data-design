d05f8e640770df93f0e01341bcade951b381e760
Ryan Mills <millselonius@gmail.com>
2020-05-05 22:12:07 -0600

    initial commit

--- sql/letterboxd-review.sql
+++ sql/letterboxd-review.sql
@@ -4,9 +4,9 @@ DROP TABLE IF EXISTS profile;
 
 CREATE TABLE profile (
     profileId BINARY(16) NOT NULL,
-    profileActivationToken CHAR(32)
+    profileActivationToken CHAR(32),
     profileEmail VARCHAR(128) NOT NULL,
-    profileHandle VARCHAR(32) NOT NULL
+    profileHandle VARCHAR(32) NOT NULL,
     UNIQUE(profileHandle),
     UNIQUE(profileEmail),
     PRIMARY KEY (profileId)
@@ -15,7 +15,7 @@ CREATE TABLE profile (
 CREATE TABLE film (
     filmId BINARY(16) NOT NULL,
     filmName VARCHAR(256) NOT NULL,
-    filmCover VARBINARY(MAX)
+    filmCover VARBINARY(MAX),
     PRIMARY KEY (filmId)
 );
 
